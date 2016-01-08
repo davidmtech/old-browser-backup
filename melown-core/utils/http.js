4b584ce463e3d038f62edab95428498db979341a
Tomáš Kavan <tomas.kavan@citationtech.net>
2016-01-08 15:22:33 +0100

    New version of async JSON loader.

--- melown-core/utils/http.js
+++ melown-core/utils/http.js
@@ -24,3 +24,44 @@ Melown.Http.imageFactory = function(url_, onload_, onerror_) {
     Melown.Http.loadImageFormUrl(image_, url_);
     return image_;
 };
+
+Melown.Http.loadJson = function(url_, onload_, onerror_) {
+    var onload_ = onload_ || function(){}
+    var onerror_ = onerror_ || function(){}
+
+    var xhr_ = new XMLHttpRequest();
+
+    xhr_.onreadystatechange = function() {
+        switch (xhr_.readyState) {
+            case 0 : // unset
+            case 1 : // opened
+            case 2 : // headers received
+            case 3 : // loading
+            break;
+            case 4 : // done
+                // check status code
+                if (xhr_.status === 404) {
+                    var err_ = new Error('JSON file not found at ' + url_);
+                    err_.code = 404
+                    onerror_(err_);
+                    break;
+                }
+
+                try {
+                    var parsed_ = JSON.parse(xhr_.response);
+                } catch(e) {
+                    var err_ = new Error('Can\'t parse JSON file not found at ' 
+                                         + url_ + '. Error: ' + e);
+                    onerror_(err_);
+                    break;
+                }
+
+                onload_(parsed_);
+        }
+    }   
+
+    xhr_.open('GET', url_, true);
+    xhr_.send('');
+
+    return xhr_;
+}
